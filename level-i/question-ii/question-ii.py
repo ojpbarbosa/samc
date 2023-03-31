0ae77a07a4bd7f6895dc110c350b522fac1689df
ojpbarbosa <joaopedrofbarbosa@protonmail.com>
2023-03-31 18:58:43 -0300

    chore: finish question ii

--- level-i/question-ii/question-ii.py
+++ level-i/question-ii/question-ii.py
@@ -96,22 +96,21 @@ class CellularAutomata:
 
 class Pathfinder:
     def __init__(self, origin: tuple, destination: tuple):
-        self.path = []                  # initialize an empty path
-        self.explorers = []             # initialize an empty list of explorers
+        self.path = []
+        self.explorers = []
 
-        self.origin = origin            # set the origin point
-        self.destination = destination  # set the destination point
+        self.origin = origin
+        self.destination = destination
 
-        # add the starting point as an initial explorer
         self.explorers.append([origin])
 
-        self.MAXIMUM_EXPLORERS = 100       # set the maximum number of explorers
+        self.MAXIMUM_EXPLORERS = 100
 
     def explore(self, matrix):
-        new_explorers = []              # initialize a new list of explorers
+        new_explorers = []
 
         def valid_explorer(explorer):
-            # check if the new explorer is not the same as any previous ones
+
             for new_explorer in new_explorers:
                 if explorer == None or new_explorer == None:
                     return False
@@ -122,22 +121,18 @@ class Pathfinder:
             return True
 
         for explorer in self.explorers[:]:
-            # get the coordinates of the last point of the explorer
             explorer_x, explorer_y = explorer[-1]
 
             available_movements = self.available_movements(
-                explorer_x, explorer_y, matrix)   # get the available movements for the current point
+                explorer_x, explorer_y, matrix)
 
-            # if there are no available movements, skip this explorer
             if len(available_movements) == 0:
                 continue
 
-            # if there is only one available movement, add it to the explorer
             elif len(available_movements) == 1:
                 new_explorer = explorer + [available_movements[0]]
                 new_explorers.append(new_explorer)
 
-            # if there are multiple available movements, clone the explorer for each one
             elif len(available_movements) > 1:
                 for movement in available_movements:
                     clone_explorer = explorer.copy()
@@ -147,51 +142,41 @@ class Pathfinder:
                     if valid_explorer(new_explorer):
                         new_explorers.append(new_explorer)
 
-        # remove any None values from the list of explorers
         new_explorers = list(filter(None, new_explorers))
-        # clear the previous list of explorers
+
         self.explorers.clear()
-        # set the new list of explorers
         self.explorers = new_explorers.copy()
 
-        # check if there are any explorers that moved
         if len(self.explorers) == 0:
             return
 
-        # limit to N closest explorers to destination
-        # sort the explorers based on their distance to the destination
         self.explorers.sort(key=lambda x: self.distance(x[-1]))
-        # keep only the closest self.MAXIMUM_EXPLORERS explorers
         self.explorers = self.explorers[:self.MAXIMUM_EXPLORERS]
 
     def available_movements(self, x, y, matrix):
-        available_movements = []  # initialize a list of available movements
-        # get the number of columns in the matrix
-        column_count = matrix.shape[0]
-        row_count = matrix.shape[1]  # get the number of rows in the matrix
+        available_movements = []
 
-        # check if the right movement is available
+        column_count = matrix.shape[0]
+        row_count = matrix.shape[1]
+
         if x + 1 < column_count and (matrix[x + 1, y] == 0 or matrix[x + 1, y] == 4):
             if matrix[x + 1, y] == 4:
                 return [(x + 1, y)]
 
             available_movements.append((x + 1, y))
 
-        # check if the left movement is available
         if x - 1 >= 0 and (matrix[x - 1, y] == 0 or matrix[x - 1, y] == 4):
             if matrix[x - 1, y] == 4:
                 return [(x - 1, y)]
 
             available_movements.append((x - 1, y))
 
-        # check if the down movement is available
         if y + 1 < row_count and (matrix[x, y + 1] == 0 or matrix[x, y + 1] == 4):
             if matrix[x, y + 1] == 4:
                 return [(x, y + 1)]
 
             available_movements.append((x, y + 1))
 
-        # check if the up movement is available
         if y - 1 >= 0 and (matrix[x, y - 1] == 0 or matrix[x, y - 1] == 4):
             if matrix[x, y - 1] == 4:
                 return [(x, y - 1)]
@@ -201,7 +186,6 @@ class Pathfinder:
         return available_movements
 
     def distance(self, movement):
-        # manhattan distance calculation
         return abs(self.destination[0] - movement[0]) + abs(self.destination[1] - movement[1])
 
     def move(self, matrix):
@@ -257,7 +241,7 @@ if __name__ == '__main__':
     print(pf.path_to_string())
 
     print(f'Found in {end_time - start_time}s')
-    print(f'Path length: {len(pf.path)}')
+    print(f'Path length: {len(pf.path) - 1}')
 
     with open(f'{matrix}-{time()}.txt', 'w') as file:
         file.write(pf.path_to_string())
